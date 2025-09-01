26e4a1e55af6ef0259e9b75ca0249413aed846f3
sajjadkiyani <kiyani.programmer@gmail.com>
2025-09-01 21:52:40 +0330

    add file admin and dashboardController

---
+++ modules/admin/dashboard/controllers/DashboardController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace modules\admin\dashboard\controllers;
+
+use src\Controller;
+
+class DashboardController extends Controller
+{
+
+    public function indexAction()
+    {
+            echo "welcome to dashboard controller";
+    }
+}
---
+++ public/admin/index.php
@@ -0,0 +1,36 @@
+<?php
+use model\Router;
+use modules\admin\dashboard\controllers\DashboardController;
+use modules\page\Controllers\PageController;
+use src\DataBaseConnection;
+define('ROOT_PATH', dirname(__DIR__).DIRECTORY_SEPARATOR.'../'.DIRECTORY_SEPARATOR);
+define('VIEW_PATH', dirname(__DIR__).DIRECTORY_SEPARATOR.'view'.DIRECTORY_SEPARATOR);
+define('MODULE_PATH', dirname(__DIR__).DIRECTORY_SEPARATOR.'../modules'.DIRECTORY_SEPARATOR);
+include(ROOT_PATH."src/Controller.php");
+include(ROOT_PATH."src/Template.php");
+include(ROOT_PATH."src/Entity.php");
+include(ROOT_PATH."model/Router.php");
+include(ROOT_PATH."src/DataBaseConnection.php");
+include(MODULE_PATH."page/models/Page.php");
+
+DataBaseConnection::getInstance();
+DataBaseConnection::connect('localhost','cms_php','root','');
+
+$action = $_GET['action'];
+$module = $_GET['module'] ?? $_POST['module'] ?? null;
+$action = $_GET['action'] ?? $_POST['action'] ?? null;
+
+    if ($module == "about-us") {
+        include("../view/about-us.php");
+    } else if ($module == "contact-us") {
+        include("../view/contact-us.php");
+    } else if ($module == "home") {
+        include("../view/home.php");
+    } else if ($module == "dashboard") {
+        $ControllerName = MODULE_PATH."admin/dashboard/controllers/DashboardController.php";
+        include($ControllerName);
+        $dashboardObj = new DashboardController();
+        $dashboardObj->runAction($action);
+    } else if ($module == "home") {
+        include("../controller/home.php");
+    }
